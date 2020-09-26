42f4d1e50964c50be43b1605d8e91c638f469389
anirudh atodaria <anirudhatodaria11@gmail.com>
2020-09-26 16:21:34 +0530

    only owner(the one who posted) can update/delete (added code for that check)

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -41,7 +41,8 @@ exports.editComment = async (req, res, next) => {
     const comment = await Comment.findByPk(req.params.id);
     if (!comment) res.status(404).send({ error: 'comment not found' });
     if (comment.userUserId !== user.user_id) res.status(401).send({ error: 'unauthorized' });
-    //TODO - INCOMPLETE
+    await Comment.update({ description }, { where: { comment_id: comment.comment_id } });
+    res.send();
   } catch(e) {
     console.log(e);
     res.status(500).send({ error: e.message });
--- routes/comment.js
+++ routes/comment.js
@@ -11,4 +11,7 @@ route.post('/comment/:id', auth, commentController.addComment);
 //delete comment (using commentid) AUTH ONLY
 route.delete('/comment/:id', auth, commentController.deleteComment);
 
+//update comment (using commentId) AUTH ONLY
+route.patch('/comment/:id', auth, commentController.editComment);
+
 module.exports = route;
